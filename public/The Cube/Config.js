1474367be3dac88790a4df960df08459975dbf34
Aidan Garton <amgb2018@mymail.pomona.edu>
2020-05-11 15:17:34 -0500

    Organized files, deleted unnecesary libraries

---
+++ public/The Cube/Config.js
@@ -0,0 +1,86 @@
+class Config {
+  constructor(sides) {
+    if (sides) {
+      this.sides = sides;
+    } else {
+      this.sides = [0, 1, 2, 3, 4, 5];
+    }
+  }
+
+  get(side) {
+    return this.sides[side];
+  }
+
+  copy() {
+    let temp = [];
+    arrayCopy(this.sides, temp);
+
+    return new Config(temp);
+  }
+
+  rotateZ(CW) {
+    if (CW) {
+      let temp = this.sides[U];
+      this.sides[U] = this.sides[L];
+      this.sides[L] = this.sides[D];
+      this.sides[D] = this.sides[R];
+      this.sides[R] = temp;
+    } else {
+      let temp = this.sides[U];
+      this.sides[U] = this.sides[R];
+      this.sides[R] = this.sides[D];
+      this.sides[D] = this.sides[L];
+      this.sides[L] = temp;
+    }
+  }
+
+  rotateY(CW) {
+    if (CW) {
+      let temp = this.sides[F];
+      this.sides[F] = this.sides[R];
+      this.sides[R] = this.sides[B];
+      this.sides[B] = this.sides[L];
+      this.sides[L] = temp;
+    } else {
+      let temp = this.sides[R];
+      this.sides[R] = this.sides[F];
+      this.sides[F] = this.sides[L];
+      this.sides[L] = this.sides[B];
+      this.sides[B] = temp;
+    }
+  }
+
+  rotateX(CW) {
+    if (CW) {
+      let temp = this.sides[U];
+      this.sides[U] = this.sides[F];
+      this.sides[F] = this.sides[D];
+      this.sides[D] = this.sides[B];
+      this.sides[B] = temp;
+    } else {
+      let temp = this.sides[F];
+      this.sides[F] = this.sides[U];
+      this.sides[U] = this.sides[B];
+      this.sides[B] = this.sides[D];
+      this.sides[D] = temp;
+    }
+  }
+}
+
+class Index {
+  constructor(a, b) {
+    this.a = a;
+    this.b = b;
+  }
+}
+
+let order = [
+  new Index(-1, -1),
+  new Index(0, -1),
+  new Index(1, -1),
+  new Index(1, 0),
+  new Index(1, 1),
+  new Index(0, 1),
+  new Index(-1, 1),
+  new Index(-1, 0),
+];
